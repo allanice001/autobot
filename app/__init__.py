daeed2c755e60561e798f167db32ce9033581ae3
dragonmaster <dragonmaster@acmeaws.com>
2016-09-06 08:41:27 +0200

    Gitignore updated

--- app/__init__.py
+++ app/__init__.py
@@ -38,44 +38,45 @@ class Main(flask.views.MethodView):
         pass
 
 
-api.add_resource(GetHostVarsAPI, '/api/hosts/<string:hostname>/vars')
-api.add_resource(GetHostGroupsAPI, '/api/hosts/<string:hostname>/groups')
-api.add_resource(GetHostsSearchAPI, '/api/hosts/search/<string:search_term>')
-api.add_resource(HostsAPI, '/api/hosts')
+api.add_resource(GetHostVarsAPI, '/api/ironhide/hosts/<string:hostname>/vars')
+api.add_resource(GetHostGroupsAPI, '/api/ironhide/hosts/<string:hostname>/groups')
+api.add_resource(GetHostsSearchAPI, '/api/ironhide/hosts/search/<string:search_term>')
+api.add_resource(HostsAPI, '/api/ironhide/hosts')
 
-api.add_resource(GetGroupChildrenAPI, '/api/groups/<string:groupname>/children')
-api.add_resource(GetGroupVarsAPI, '/api/groups/<string:groupname>/vars')
-api.add_resource(GetGroupHostsAPI, '/api/groups/<string:groupname>/hosts')
-api.add_resource(GetGroupsSearchAPI, '/api/groups/search/<string:search_term>')
-api.add_resource(GroupsAPI, '/api/groups')
+api.add_resource(GetGroupChildrenAPI, '/api/ironhide/groups/<string:groupname>/children')
+api.add_resource(GetGroupVarsAPI, '/api/ironhide/groups/<string:groupname>/vars')
+api.add_resource(GetGroupHostsAPI, '/api/ironhide/groups/<string:groupname>/hosts')
+api.add_resource(GetGroupsSearchAPI, '/api/ironhide/groups/search/<string:search_term>')
+api.add_resource(GroupsAPI, '/api/ironhide/groups')
 
-# api.add_resource(TagsAPI,'/api/tags')
+# api.add_resource(TagsAPI,'/api/ironhide/tags')
 
-api.add_resource(RolesAPI,'/api/roles')
-api.add_resource(GetRoleTagsAPI,'/api/roles/<string:rolename>/tags')
-api.add_resource(GetRolesSearchAPI,'/api/roles/search/<string:search_term>')
+api.add_resource(RolesAPI,'/api/ironhide/roles')
+api.add_resource(GetRoleTagsAPI,'/api/ironhide/roles/<string:rolename>/tags')
+api.add_resource(GetRolesSearchAPI,'/api/ironhide/roles/search/<string:search_term>')
 
 app.add_url_rule('/', view_func=Main.as_view('index'), methods=['GET', 'POST'])
+#app.add_url_rule('/ironhide/', view_func=Main.as_view('index'), methods=['GET', 'POST'])
 
-app.add_url_rule('/addhost', view_func=AddHost.as_view('addhost'), methods=['GET', 'POST'])
-app.add_url_rule('/gethostinfo', view_func=GetHost.as_view('gethostinfo'), methods=['GET', 'POST'])
-app.add_url_rule('/getallhosts', view_func=GetAllHosts.as_view('getallhosts'), methods=['POST','GET'])
-app.add_url_rule('/edithost', view_func=EditHost.as_view('edithost'), methods=['GET', 'POST'])
-app.add_url_rule('/edithostsubmit', view_func=EditHostSubmit.as_view('edithostsubmit'), methods=['GET', 'POST'])
+app.add_url_rule('/ironhide/addhost', view_func=AddHost.as_view('addhost'), methods=['GET', 'POST'])
+app.add_url_rule('/ironhide/gethostinfo', view_func=GetHost.as_view('gethostinfo'), methods=['GET', 'POST'])
+app.add_url_rule('/ironhide/getallhosts', view_func=GetAllHosts.as_view('getallhosts'), methods=['POST','GET'])
+app.add_url_rule('/ironhide/edithost', view_func=EditHost.as_view('edithost'), methods=['GET', 'POST'])
+app.add_url_rule('/ironhide/edithostsubmit', view_func=EditHostSubmit.as_view('edithostsubmit'), methods=['GET', 'POST'])
 
-app.add_url_rule('/editgroup', view_func=EditGroup.as_view('editgroup'), methods=['GET', 'POST'])
-app.add_url_rule('/editgroupsubmit', view_func=EditGroupSubmit.as_view('editgroupsubmit'), methods=['GET', 'POST'])
-app.add_url_rule('/getgroup', view_func=GetGroup.as_view('getgroup'), methods=['GET', 'POST'])
-app.add_url_rule('/getallgroups', view_func=GetAllGroups.as_view('getallgroups'), methods=['GET', 'POST'])
-app.add_url_rule('/addgroups', view_func=AddGroup.as_view('addgroup'), methods=['GET', 'POST'])
+app.add_url_rule('/ironhide/editgroup', view_func=EditGroup.as_view('editgroup'), methods=['GET', 'POST'])
+app.add_url_rule('/ironhide/editgroupsubmit', view_func=EditGroupSubmit.as_view('editgroupsubmit'), methods=['GET', 'POST'])
+app.add_url_rule('/ironhide/getgroup', view_func=GetGroup.as_view('getgroup'), methods=['GET', 'POST'])
+app.add_url_rule('/ironhide/getallgroups', view_func=GetAllGroups.as_view('getallgroups'), methods=['GET', 'POST'])
+app.add_url_rule('/ironhide/addgroups', view_func=AddGroup.as_view('addgroup'), methods=['GET', 'POST'])
 
 # app.add_url_rule('/addtag', view_func=AddTag.as_view('addtag'), methods=['GET', 'POST'])
 # app.add_url_rule('/getalltags', view_func=GetAllTags.as_view('getalltags'), methods=['GET', 'POST'])
 # app.add_url_rule('/gettaginfo', view_func=GetTag.as_view('gettaginfo'), methods=['GET', 'POST'])
 
-app.add_url_rule('/addrole', view_func=AddRole.as_view('addrole'), methods=['GET', 'POST'])
-app.add_url_rule('/getroleinfo', view_func=GetRole.as_view('getroleinfo'), methods=['GET', 'POST'])
-app.add_url_rule('/getallroles', view_func=GetAllRoles.as_view('getallroles'), methods=['GET', 'POST'])
+app.add_url_rule('/ironhide/addrole', view_func=AddRole.as_view('addrole'), methods=['GET', 'POST'])
+app.add_url_rule('/ironhide/getroleinfo', view_func=GetRole.as_view('getroleinfo'), methods=['GET', 'POST'])
+app.add_url_rule('/ironhide/getallroles', view_func=GetAllRoles.as_view('getallroles'), methods=['GET', 'POST'])
 
-app.add_url_rule('/remove', view_func=Remove.as_view('remove'), methods=['GET', 'POST'])
+app.add_url_rule('/ironhide/remove', view_func=Remove.as_view('remove'), methods=['GET', 'POST'])
 
