6d39234e2970359daafbe85f18ab5b6fd4d26c14
allan <allanice001@gmail.com>
2016-09-07 05:44:28 +0200

    Adding templates for ironhide, bumblebee and optimus prime

--- app/modules/host/edit_host.py
+++ app/modules/host/edit_host.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
-import flask
-import flask.views
+from flask import request, flash, render_template, redirect, url_for
+from flask.views import MethodView
 import yaml
 import json
 from app import common
@@ -8,24 +8,24 @@ from app import common
 from app.common import db
 
 
-class EditHost(flask.views.MethodView):
+class EditHost(MethodView):
 
     def post(self):
-        hostname = str(flask.request.form['p_get'])
+        hostname = str(request.form['p_get'])
         if len(hostname) == 0:
-            flask.flash('empty hostname given')
-            return flask.render_template('edithost.html')
+            flash('empty hostname given')
+            return render_template('edithost.html')
         elif not common.host_exists(hostname):
-            flask.flash('hostname not found')
-            return flask.render_template('edithost.html')
+            flash('hostname not found')
+            return render_template('edithost.html')
         else:
             result = self.get_hostinfo(hostname)
             available_groups = self.get_availablegroups()
             groups = self.get_hostgroups(hostname)
-            return flask.render_template('edithost.html', host=hostname, result=result, groups=groups, available_groups=available_groups)
+            return render_template('edithost.html', host=hostname, result=result, groups=groups, available_groups=available_groups)
 
     def get(self):
-        return flask.render_template('edithost.html')
+        return render_template('edithost.html')
 
     def get_hostinfo(self, hostname):
         result = db.hosts.find({"hostname": hostname}).distinct("vars")
@@ -37,31 +37,32 @@ class EditHost(flask.views.MethodView):
             return None
 
     def get_hostgroups(self, hostname):
-        '''retrieve all groups the host is a member of'''
+        # retrieve all groups the host is a member of
         result = common.get_all_groups_for_host(hostname)
         groups = [ item["groupname"] for item in result]
         return groups
 
     def get_availablegroups(self):
-        ''' return all groups this host is not a member of'''
+        # return all groups this host is not a member of
         allgroups = common.get_all_groups()
         # build compared list
-        hostname = str(flask.request.form['p_get'])
+        hostname = str(request.form['p_get'])
         groups = self.get_hostgroups(hostname)
         s = set(groups)
         availablegroups = [ x for x in allgroups if x not in s ]
         return availablegroups
 
-class EditHostSubmit(flask.views.MethodView):
+
+class EditHostSubmit(MethodView):
 
     def post(self):
-        hostname = str(flask.request.form['p_get2'])
+        hostname = str(request.form['p_get2'])
         self.update_host(hostname)
         self.update_groups(hostname)
-        return flask.redirect('getallhosts')
+        return redirect(url_for('getallhosts'))
 
     def update_host(self,hostname):
-            yamlvars = flask.request.form['ehyaml']
+            yamlvars = request.form['ehyaml']
             try:
                 y = yaml.load(yamlvars)
             except yaml.YAMLError, exc:
@@ -74,7 +75,7 @@ class EditHostSubmit(flask.views.MethodView):
     def update_groups(self,hostname):
             g = EditHost()
             current_groups = g.get_hostgroups(hostname)
-            updated_groups = flask.request.form.getlist('groupselect')
+            updated_groups = request.form.getlist('groupselect')
             addh = set(current_groups)
             remh = set(updated_groups)
             add_hosts_group = [ x for x in updated_groups if x not in addh ]
@@ -86,4 +87,4 @@ class EditHostSubmit(flask.views.MethodView):
             pass
 
     def get(self):
-        return flask.render_template('edithost.html')
+        return render_template('edithost.html')
